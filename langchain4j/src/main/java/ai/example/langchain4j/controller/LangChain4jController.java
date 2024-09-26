bb2891764c255fed9425eb2f4d3c1aa64400211a
victor <451745260cao>
2024-09-26 21:11:11 +0800

    langchain4j 添加记忆功能

--- langchain4j/src/main/java/ai/example/langchain4j/controller/LangChain4jController.java
+++ langchain4j/src/main/java/ai/example/langchain4j/controller/LangChain4jController.java
@@ -1,9 +1,14 @@
 package ai.example.langchain4j.controller;
 
+import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.ollama.OllamaEmbeddingModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.MemoryId;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -11,18 +16,64 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/langchain4jChat")
 public class LangChain4jController {
+
     OllamaChatModel chatModel;
+
     OllamaEmbeddingModel embeddingModel;
+
     LangChain4jController(OllamaChatModel chatModel, OllamaEmbeddingModel embeddingModel) {
         this.chatModel = chatModel;
         this.embeddingModel = embeddingModel;
     }
 
+    interface Assistant {
+
+        String chat(@MemoryId String memoryId, @dev.langchain4j.service.UserMessage String userMessage);
+    }
+
+    Assistant assistant = AiServices.builder(Assistant.class)
+            .chatLanguageModel(chatModel)
+            .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
+            .build();
+
+    /**
+     * 聊天
+     * @param message
+     * @return
+     */
     @GetMapping("/assistant")
     public String assistant(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
         return chatModel.generate(new SystemMessage("请全部用英语回答"),new UserMessage(message)).content().text();
     }
 
+    /**
+     * 有记忆的问答
+     * @param message
+     * @return
+     */
+    @GetMapping("/assistant2")
+    public String assistant2(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
+      ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+      chatMemory.add(new UserMessage(message));
+      String  result = chatModel.generate(chatMemory.messages()).content().text();
+      chatMemory.add(new AiMessage(result));
+      return result;
+    }
+
+    @GetMapping("/assistant3")
+    public String assistant3(@RequestParam(value = "memoryId", defaultValue = "1")String memoryId,@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
+
+        String result = assistant.chat(memoryId,message);
+
+        return result;
+    }
+
+
+    /**
+     * 生成embedding 属据
+     * @param message
+     * @return
+     */
     @GetMapping("/embd")
     public String embd(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
         return embeddingModel.embed(message).content().toString();
