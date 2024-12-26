21b0f2dbc9d15a59e89db937d4d9da77c95b1de4
victor <y.cao@zy-healthtech.com>
2024-12-26 11:35:14 +0800

    添加langchain4j的视觉中的图生文调用

--- langchain4j/src/main/java/ai/example/langchain4j/controller/LangChain4jController.java
+++ langchain4j/src/main/java/ai/example/langchain4j/controller/LangChain4jController.java
@@ -1,11 +1,10 @@
 package ai.example.langchain4j.controller;
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.message.*;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.ollama.OllamaEmbeddingModel;
 import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
@@ -13,9 +12,16 @@ import dev.langchain4j.model.output.Response;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.spring.AiService;
+import okhttp3.internal.ws.RealWebSocket;
 import org.springframework.http.MediaType;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 
 @RestController
@@ -57,7 +63,7 @@ public class LangChain4jController {
     @AiService
     interface SteamAssistant {
 
-      void chat(@MemoryId String memoryId, @dev.langchain4j.service.UserMessage String userMessage);
+        void chat(@MemoryId String memoryId, @dev.langchain4j.service.UserMessage String userMessage);
     }
 
 
@@ -74,9 +80,10 @@ public class LangChain4jController {
 
     /**
      * 流式访问
+     *
      * @return
      */
-    @GetMapping(value = "/steamAssistant",produces = MediaType.APPLICATION_NDJSON_VALUE)
+    @GetMapping(value = "/steamAssistant", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<String> steamAssistant(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
         return Flux.create(emitter -> {
             streamingChatModel.generate(message, new StreamingResponseHandler<AiMessage>() {
@@ -101,22 +108,50 @@ public class LangChain4jController {
 
     /**
      * 有记忆的问答
+     *
      * @param message
      * @return
      */
     @GetMapping("/assistant2")
     public String assistant2(@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
-      ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
-      chatMemory.add(new UserMessage(message));
-      String  result = chatModel.generate(chatMemory.messages()).content().text();
-      chatMemory.add(new AiMessage(result));
-      return result;
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        chatMemory.add(new UserMessage(message));
+        String result = chatModel.generate(chatMemory.messages()).content().text();
+        chatMemory.add(new AiMessage(result));
+        return result;
     }
 
-    @GetMapping("/assistant3")
-    public String assistant3(@RequestParam(value = "memoryId", defaultValue = "1")String memoryId,@RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
 
-        String result = assistant.chat(memoryId,message);
+    @PostMapping("/assistant4")
+
+    public String assistant4(@RequestParam(value = "file") MultipartFile file, @RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
+
+        String Base64Image = null;
+        try {
+            Base64Image = Base64.getEncoder().encodeToString(file.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Base64Image != null) {
+            List<Content> contents = new java.util.ArrayList<>();
+            contents.add(new TextContent(message));
+            // 文件的格式
+            String mimeType="jpg";
+            contents.add(new ImageContent(Base64Image,mimeType));
+
+            return chatModel.generate(new UserMessage(contents)).content().text();
+        }
+
+
+        return "success";
+    }
+
+
+    @GetMapping("/assistant3")
+    public String assistant3(@RequestParam(value = "memoryId", defaultValue = "1") String memoryId, @RequestParam(value = "message", defaultValue = "What is the time now?") String message) {
+
+        String result = assistant.chat(memoryId, message);
 
         return result;
     }
@@ -124,6 +159,7 @@ public class LangChain4jController {
 
     /**
      * 生成embedding 属据
+     *
      * @param message
      * @return
      */
